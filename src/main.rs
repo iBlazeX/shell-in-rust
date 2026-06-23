06b8b9dcf8b0e8481c24897c89ee259cfffa5733
Shubham Kumar <85124169+iBlazeX@users.noreply.github.com>
2026-06-24 03:34:30 +0530

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,7 @@ fn main() {
         io::stdout().flush().unwrap();
         let mut command = String::new();
         io::stdin().read_line(&mut command).unwrap();
-        command.trim().to_string();
-        match command.as_str() {
+        match command.trim() {
             "exit" => break,
             "echo" => println!("{}", &command[5..]),
             _ => println!("{}: command not found", command.trim()),
