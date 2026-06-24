d97c8e11c029676a2ee3b5ad0cd21ce88683a0fc
Shubham Kumar <85124169+iBlazeX@users.noreply.github.com>
2026-06-25 03:38:23 +0530

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,29 @@
+use ::std::env;
+use std::fs::Metadata;
 #[allow(unused_imports)]
 use std::io::{self, Write};
+use std::os::unix::fs::PermissionsExt;
+use std::path;
+use std::{env::split_paths, fs::metadata, path::PathBuf};
+
+fn is_exec(meta: &Metadata) -> bool {
+    meta.permissions().mode() & 0o111 != 0
+}
+
+pub fn find_exec(args: &str) -> Option<PathBuf> {
+    let path = var_os("PATH").unwrap();
+    for dir in split_paths(&path) {
+        let candidate = dir.join(args);
+        if !candidate.is_file() {
+            continue;
+        }
+        let meta = metadata(&candidate).unwrap();
+        if is_exec(&meta) {
+            return Some(candidate);
+        }
+    }
+    None
+}
 
 fn main() {
     loop {
@@ -16,7 +40,11 @@ fn main() {
             "echo" => println!("{}", args),
             "type" => match args {
                 "exit" | "echo" | "type" => println!("{} is a shell builtin", args),
-                _ => println!("{}: not found", args),
+                _ => {
+                	match find_exec(args) {
+               			Some(path) => println!("{} is {}", args, args),
+                 		None => println!("{}: not found", args),
+                }
             },
             _ => println!("{}: command not found", cmd),
         }
