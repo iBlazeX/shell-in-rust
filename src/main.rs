3e7026f7020059f9d2c1d2bd72e7709d762f96df
Shubham Kumar <85124169+iBlazeX@users.noreply.github.com>
2026-06-26 17:36:58 +0530

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
+use std::fs::metadata;
 #[allow(unused_imports)]
 use std::{
-    env,
-    fs::{self, Metadata},
+    env, fs,
     io::{self, Write},
     mem,
     os::unix::fs::PermissionsExt,
@@ -9,79 +9,6 @@ use std::{
     path::{self, PathBuf},
     process::Command,
 };
-
-fn is_exec(meta: &Metadata) -> bool {
-    meta.permissions().mode() & 0o111 != 0
-}
-
-pub fn find_exec(cmd: &str) -> Option<PathBuf> {
-    let path = env::var_os("PATH").unwrap();
-    for dir in env::split_paths(&path) {
-        let candidate = dir.join(cmd);
-        if !candidate.is_file() {
-            continue;
-        }
-        let meta = fs::metadata(&candidate).unwrap();
-        if is_exec(&meta) {
-            return Some(candidate);
-        }
-    }
-    None
-}
-
-fn tokenize(line: &str) -> Vec<String> {
-    let mut token = Vec::new();
-    let mut current = String::new();
-    let mut in_quotes = false;
-    let mut in_db_quotes = false;
-    let mut back = false;
-
-    for c in line.chars() {
-        if back {
-            current.push(c);
-            back = false;
-            continue;
-        }
-        match c {
-            '\\' => {
-                if in_quotes {
-                    current.push(c);
-                } else {
-                    back = true;
-                }
-            }
-            '\'' => {
-                if !in_db_quotes {
-                    in_quotes = !in_quotes;
-                } else {
-                    current.push(c);
-                }
-            }
-            '\"' => {
-                if !in_quotes {
-                    in_db_quotes = !in_db_quotes;
-                } else {
-                    current.push(c);
-                }
-            }
-            ' ' => {
-                if in_quotes || in_db_quotes {
-                    current.push(c);
-                } else if !current.is_empty() {
-                    token.push(mem::take(&mut current));
-                }
-            }
-            _ => current.push(c),
-        }
-    }
-    if back {
-        current.push('\\');
-    }
-    if !current.is_empty() {
-        token.push(current);
-    }
-    token
-}
 
 fn main() {
     loop {
@@ -92,14 +19,20 @@ fn main() {
         if command.trim().is_empty() {
             continue;
         }
-        let token: Vec<String> = tokenize(command.trim());
-        let (cmd, args) = (&token[0], &token[1..]);
+        let (cmd, args, stout) = tokenize(command.trim());
+        let mut file;
+        let out: &mut dyn Write = if let Some(path) = stout {
+            file = File::create(path).unwrap();
+            &mut file
+        } else {
+            &mut io::stdout()
+        };
         match cmd.as_str() {
             "exit" => break,
-            "echo" => println!("{}", args.join(" ")),
+            "echo" => writeln!(out, "{}", args.join(" ")).unwrap(),
             "pwd" => {
                 let cwd = env::current_dir().unwrap();
-                println!("{}", cwd.display());
+                writeln!(out, "{}", cwd.display()).unwrap();
             }
             "cd" => {
                 if args.is_empty() {
@@ -123,15 +56,19 @@ fn main() {
                 let arg = &args[0];
                 match arg.as_str() {
                     "exit" | "echo" | "type" | "pwd" | "cd" => {
-                        println!("{} is a shell builtin", arg)
+                        writeln!(out, "{} is a shell builtin", arg).unwrap();
                     }
                     _ => match find_exec(arg) {
-                        Some(path) => println!("{} is {}", arg, path.display()),
+                        Some(path) => writeln!(out, "{} is {}", arg, path.display()).unwrap(),
                         None => println!("{}: not found", arg),
                     },
                 }
             }
-            _ => match find_exec(cmd) {
+            "cat" => match fs::read_to_string(&args[0]) {
+                Ok(content) => writeln!(out, "{}", content).unwrap(),
+                Err(_) => println!("cat: {}: No such file or directory", args[0]),
+            },
+            _ => match find_exec(cmd.as_str()) {
                 Some(path) => {
                     Command::new(path).arg0(cmd).args(args).status().unwrap();
                 }
@@ -139,4 +76,97 @@ fn main() {
             },
         }
     }
+}
+
+fn is_exec(meta: &Metadata) -> bool {
+    meta.permissions().mode() & 0o111 != 0
+}
+
+fn find_exec(cmd: &str) -> Option<PathBuf> {
+    let path = env::var_os("PATH").unwrap();
+    for dir in env::split_paths(&path) {
+        let candidate = dir.join(cmd);
+        if !candidate.is_file() {
+            continue;
+        }
+        let meta = fs::metadata(&candidate).unwrap();
+        if is_exec(&meta) {
+            return Some(candidate);
+        }
+    }
+    None
+}
+
+fn tokenize(line: &str) -> (String, Vec<String>, Option<String>) {
+    let mut token = Vec::new();
+    let mut current = String::new();
+    let mut in_quotes = false;
+    let mut in_db_quotes = false;
+    let mut backslash = false;
+    let mut expect_stdout = false;
+    let mut stout = None;
+
+    for c in line.chars() {
+        if backslash {
+            current.push(c);
+            backslash = false;
+            continue;
+        }
+        match c {
+            '\\' => {
+                if in_quotes {
+                    current.push(c);
+                } else {
+                    backslash = true;
+                }
+            }
+            '\'' => {
+                if !in_db_quotes {
+                    in_quotes = !in_quotes;
+                } else {
+                    current.push(c);
+                }
+            }
+            '\"' => {
+                if !in_quotes {
+                    in_db_quotes = !in_db_quotes;
+                } else {
+                    current.push(c);
+                }
+            }
+            ' ' => {
+                if in_quotes || in_db_quotes {
+                    current.push(c);
+                } else if !current.is_empty() {
+                    if expect_stdout {
+                        stout = Some(mem::take(&mut current));
+                        expect_stdout = false;
+                    } else {
+                        token.push(mem::take(&mut current));
+                    }
+                }
+            }
+            '>' => {
+                if !in_quotes && !in_db_quotes && !backslash {
+                    if !current.is_empty() {
+                        token.push(mem::take(&mut current));
+                    }
+                    expect_stdout = true;
+                } else {
+                    current.push(c);
+                }
+            }
+            _ => current.push(c),
+        }
+    }
+    if backslash {
+        current.push('\\');
+    }
+    if expect_stdout {
+        stout = Some(current);
+    } else if !current.is_empty() {
+        token.push(current);
+    }
+    let cmd = token.remove(0);
+    (cmd, token, stout)
 }
