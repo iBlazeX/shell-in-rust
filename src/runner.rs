fdd3f8ec264276e88da74e68f14206d4b23513af
Shubham Kumar <85124169+iBlazeX@users.noreply.github.com>
2026-06-27 04:01:02 +0530

    codecrafters submit [skip ci]

--- src/runner.rs
+++ src/runner.rs
@@ -15,66 +15,12 @@ pub fn run(parsed: &ParsedCmd, out: &mut dyn Write, err: &mut dyn Write) -> Shel
     } = parsed;
     match cmd.as_str() {
         "exit" => return ShellAction::Exit,
-        "echo" => writeln!(out, "{}", args.join(" ")).unwrap(),
-        "pwd" => {
-            let cwd = env::current_dir().unwrap();
-            writeln!(out, "{}", cwd.display()).unwrap();
-        }
-        "cd" => {
-            if args.is_empty() {
-                writeln!(err, "cd: No directory specified").unwrap();
-                return ShellAction::Continue;
-            }
-            if args[0] == "~" {
-                env::set_current_dir(env::home_dir().unwrap()).unwrap();
-            } else {
-                match env::set_current_dir(&args[0]) {
-                    Ok(_) => {}
-                    Err(_) => writeln!(err, "cd: {}: No such file or directory", args[0]).unwrap(),
-                }
-            }
-        }
-        "type" => {
-            if args.is_empty() {
-                writeln!(out, "type: missing argument").unwrap();
-                return ShellAction::Continue;
-            }
-            let arg = &args[0];
-            match arg.as_str() {
-                "exit" | "echo" | "type" | "pwd" | "cd" => {
-                    writeln!(out, "{} is a shell builtin", arg).unwrap();
-                }
-                _ => match find_exec(arg) {
-                    Some(path) => writeln!(out, "{} is {}", arg, path.display()).unwrap(),
-                    None => writeln!(err, "{}: not found", arg).unwrap(),
-                },
-            }
-        }
-        "cat" => {
-            for file in args {
-                match fs::read_to_string(file) {
-                    Ok(content) => write!(out, "{}", content).unwrap(),
-                    Err(_) => writeln!(err, "cat: {}: No such file or directory", file).unwrap(),
-                }
-            }
-        }
-        _ => match find_exec(cmd.as_str()) {
-            Some(path) => {
-                let mut command = Command::new(path);
-                command.arg0(cmd).args(args);
-
-                if let Some(path) = stout {
-                    let file = fs::File::create(path).unwrap();
-                    command.stdout(file);
-                }
-                if let Some(path) = &parsed.sterr {
-                    let file = fs::File::create(path).unwrap();
-                    command.stderr(file);
-                }
-                command.status().unwrap();
-            }
-            None => writeln!(err, "{}: not found", cmd).unwrap(),
-        },
+        "echo" => echo(args, out),
+        "pwd" => pwd(out),
+        "cd" => cd(args, err),
+        "type" => type_cmd(args, out, err),
+        "cat" => cat(args, out, err),
+        _ => run_external(parsed),
     }
     ShellAction::Continue
 }
@@ -96,4 +42,69 @@ fn find_exec(cmd: &str) -> Option<PathBuf> {
         }
     }
     None
+}
+
+fn echo(args: &Vec<String>, out: &mut dyn Write) {
+	writeln!(out, "{}", args.join(" ")).unwrap(),
+}
+fn pwd(out: &mut dyn Write) {
+    let cwd = env::current_dir().unwrap();
+    writeln!(out, "{}", cwd.display()).unwrap();
+}
+fn cd(args: &Vec<String>, err: &mut dyn Write, ){
+    if args.is_empty() {
+        writeln!(err, "cd: No directory specified").unwrap();
+        return ShellAction::Continue;
+    }
+    if args[0] == "~" {
+        env::set_current_dir(env::home_dir().unwrap()).unwrap();
+    } else {
+        match env::set_current_dir(&args[0]) {
+            Ok(_) => {}
+            Err(_) => writeln!(err, "cd: {}: No such file or directory", args[0]).unwrap(),
+        }
+    }
+}
+fn type_cmd(args: &Vec<String>, out: &mut dyn Write, err: &mut dyn Write){
+    if args.is_empty() {
+        writeln!(out, "type: missing argument").unwrap();
+        return ShellAction::Continue;
+    }
+    let arg = &args[0];
+    match arg.as_str() {
+        "exit" | "echo" | "type" | "pwd" | "cd" => {
+            writeln!(out, "{} is a shell builtin", arg).unwrap();
+        }
+        _ => match find_exec(arg) {
+            Some(path) => writeln!(out, "{} is {}", arg, path.display()).unwrap(),
+            None => writeln!(err, "{}: not found", arg).unwrap(),
+        },
+    }
+}
+fn cat(args: &Vec<String>, out: &mut dyn Write, err: &mut dyn Write){
+    for file in args {
+        match fs::read_to_string(file) {
+            Ok(content) => write!(out, "{}", content).unwrap(),
+            Err(_) => writeln!(err, "cat: {}: No such file or directory", file).unwrap(),
+        }
+    }
+}
+fn run_external(parsed: &ParsedCmd) {
+	match find_exec(cmd.as_str()) {
+            Some(path) => {
+                let mut command = Command::new(path);
+                command.arg0(cmd).args(args);
+
+                if let Some(path) = stout {
+                    let file = fs::File::create(path).unwrap();
+                    command.stdout(file);
+                }
+                if let Some(path) = &parsed.sterr {
+                    let file = fs::File::create(path).unwrap();
+                    command.stderr(file);
+                }
+                command.status().unwrap();
+            }
+            None => writeln!(err, "{}: not found", cmd).unwrap(),
+        }
 }
