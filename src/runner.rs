de94ed1242c515e7feea576670b9b8897322a76d
iBlazeX <iblaze.x1z@gmail.com>
2026-07-30 19:38:46 +0000

    codecrafters submit [skip ci]

--- src/runner.rs
+++ src/runner.rs
@@ -217,9 +217,13 @@ fn declare(args: &Vec<String>, out: &mut dyn Write, err: &mut dyn Write, shell: 
         return;
     }
     if let Some(arg) = args.first() {
-        if let Some((name, value)) = arg.split_once('=') {
-            shell.vars.insert(name.to_string(), value.to_string());
-            return;
+        if arg[0] != '_' || c.is_ascii_alphabetic() {
+            if let Some((name, value)) = arg.split_once('=') {
+                shell.vars.insert(name.to_string(), value.to_string());
+                return;
+            }
+        } else {
+            writeln!(err, "declare: {}: not a valid identifier", arg).unwrap();
         }
     }
 }
